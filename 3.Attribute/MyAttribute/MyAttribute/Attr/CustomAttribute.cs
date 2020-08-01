87fe5a8eef4da9eac1686f0a7c056675a3113793
panda_lqc <panda_lqc@163.com>
2020-08-01 11:49:45 +0800

    Signed-off-by: panda_lqc <panda_lqc@163.com>

--- 3.Attribute/MyAttribute/MyAttribute/Attr/CustomAttribute.cs
+++ 3.Attribute/MyAttribute/MyAttribute/Attr/CustomAttribute.cs
@@ -18,18 +18,5 @@ namespace MyAttribute.Attr
         
     }
 
-    [AttributeUsage(AttributeTargets.Property)]
-    internal class SalaryAttribute : AbstractValidateAttribute
-    {
-        public long _salary { get; set; }
-        public SalaryAttribute(long salary)
-        {
-            _salary = salary;
-        }
-
-        public override bool Validate(object value) 
-        {
-            return value != null && long.TryParse(value.ToString(), out long lValue) && lValue > _salary;
-        }
-    }
+    
 }
--- 3.Attribute/MyAttribute/MyAttribute/CustomEnum/UserState.cs
+++ 3.Attribute/MyAttribute/MyAttribute/CustomEnum/UserState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAttribute.Attr;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
