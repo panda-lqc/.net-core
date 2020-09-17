b0e7807e8c8cbedb0c313abe7186b63b54bfa7be
panda-lqc <panda_lqc@163.com>
2020-09-17 18:08:43 +0800

    Signed-off-by: panda-lqc <panda_lqc@163.com>

---
+++ a.Homework/Homeork1/Advanced.Model/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced.Model
+{
+    public class Company :BaseModel
+    {
+        public string Name { get; set; }
+        public int CreaterId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int? LastModifierId { get; set; }
+        public DateTime? LastModifyTime { get; set; }
+    }
+}
---
+++ a.Homework/Homeork1/Advanced.Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced.Model
+{
+    public class User : BaseModel
+    {
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public int CompanyId { get; set; }
+        public int State { get; set; }
+        public int UserType { get; set; }
+        public DateTime? LastLoginTime { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int? LastModifierId { get; set; }
+        public DateTime? LastModifyTime { get; set; }
+    }
+}
